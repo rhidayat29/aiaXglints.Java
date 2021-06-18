5c84e98e0bbc48416cc56b696ca6e34c27635263
rhidayat29 <rahmathidayat.imirius@gmail.com>
2021-06-18 15:33:56 +0700

    new

---
+++ try/src/oop/aia/day2/Order.java
@@ -0,0 +1,60 @@
+package oop.aia.day2;
+
+public class Order {
+	boolean isFilled;
+	double billAmount;
+	String shipping;
+
+	public Order(boolean filled, double cost, String shippingMethod) {
+		if (cost > 24.00) {
+			System.out.println("High value item!");
+		} else {
+			System.out.println("Low value item!");
+		}
+		isFilled = filled;
+		billAmount = cost;
+		shipping = shippingMethod;
+	}
+
+	public void ship() {
+		if (isFilled) {
+			System.out.println("Shipping");
+		} else {
+			System.out.println("Order not ready");
+		}
+
+		double shippingCost = calculateShipping();
+
+		System.out.println("Shipping cost: ");
+		System.out.println(shippingCost);
+	}
+
+	public double calculateShipping() {
+		double shippingCost;
+		switch (shipping) {
+		case "Regular":
+			shippingCost = 0;
+			break;
+		case "Express":    
+			shippingCost = 1.75;
+			break;
+		default:
+			shippingCost = .50; 
+		}
+		return shippingCost;
+	}
+
+	public static void main(String[] args) {
+		// create instances and call methods here!
+		Order book = new Order(true, 19.99, "Express");
+
+		book.ship();
+		System.out.println("================");
+		Order chemistrySet = new Order(false, 72.50, "Regular");
+		chemistrySet.ship();
+		System.out.println("================");
+		Order bike = new Order(true, 1000, "Regular");
+		bike.ship();
+	}
+}
+
